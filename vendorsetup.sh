b55ed2578f12d8cd1732506f9cded702c61975bd
Mario Suess <ms0281@gmx.de>
2023-05-27 10:03:56 +0200

    mido: using own fork of GrapheneOSCamera
    Change-Id: I6936775c38b8d2787c8c361f77339e59f49aeb58

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,5 +6,5 @@ echo 'Downloading latest prebuilt Camera.apk from GrapheneOS...'
 camera_dir=/tmp/GrapheneOSCamera
 camera_branch=12.1
 rm -rf $camera_dir &&
-git clone --depth=1 https://github.com/GrapheneOS/platform_external_Camera.git -b $camera_branch $camera_dir &&
+git clone --depth=1 https://github.com/ms0281/platform_external_Camera.git -b $camera_branch $camera_dir &&
 mv $camera_dir/prebuilt/Camera.apk "$DIR"/GrapheneCamera/ || exit $?
